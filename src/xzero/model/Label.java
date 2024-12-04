d30d162b919218b6de17a4f09873c17266fc63c1
Matveev Stepan Andreevich <mset321@gmail.com>
2024-12-04 21:26:18 +0300

    feat: восстановил все отношения

---
+++ src/xzero/model/Label.java
@@ -0,0 +1,38 @@
+package xzero.model;
+
+/**
+ * Метка, которую можно поместить на поле
+ */
+public class Label {
+    
+// --------------- Ячейка, которой прнадлежит метка. Задает сама ячейка -------
+    private Cell _cell = null;
+
+    public void setCell(Cell cell) {
+        if (cell == _cell) {
+            return;
+        }
+        _cell = cell;
+        cell.setLabel(this);
+    }
+    
+    public Cell cell(){
+        return _cell;
+    }
+    
+// - Игрок, которому прнадлежит метка. Метка может быть нейтральной (не принадлежать никому) -
+    
+    private Player _player = null;
+    
+    void setPlayer(Player p){
+        _player = p;
+    }
+
+    void unsetPlayer(){
+        _player = null;
+    }
+    
+    public Player player(){
+        return _player;
+    } 
+}
