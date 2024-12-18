9bb122bd415ea0e5d47224dbcdcfbacb9a136601
Matveev Stepan Andreevich <mset321@gmail.com>
2024-12-19 02:04:36 +0300

    feat: start lab8

---
+++ src/xzero/model/gameModelStrategy/ExchangePlayerNMagicCross.java
@@ -0,0 +1,16 @@
+package xzero.model.gameModelStrategy;
+
+import xzero.model.GameModel;
+
+import java.util.ArrayList;
+
+public class ExchangePlayerNMagicCross extends ChainedExchangePlayerStrategy {
+
+    private static ArrayList<>
+
+    @Override
+    public void exchangePlayer(GameModel model) {
+
+    }
+}
+~
